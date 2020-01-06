3d536c487fab7223bb6b227c547b91afc65c00ed
Yarin Ronel <y3arin@gmail.com>
2020-01-06 16:32:14 +0200

    creating xlsx file from object with lists

--- src/components/Excelerator/Excelerator.js
+++ src/components/Excelerator/Excelerator.js
@@ -1,52 +1,82 @@
-import XLSX from 'xlsx';
+import XLSX from "xlsx";
+import _ from "lodash";
 
 export default function Excelerator() {
+  function getCsvHeader(fields) {
+    const listNamesInGqlObject = getListNamesOfGqlObject(fields);
 
-    function getCsvHeader(fields) {
-        const listNamesInGqlObject = getListNamesOfGqlObject(fields);
+    const listObjects = fields.filter(({ name }) =>
+      listNamesInGqlObject.includes(name)
+    );
+    const nonListObjects = fields.filter(
+      ({ name }) => !listNamesInGqlObject.includes(name)
+    );
 
-        const listObjects = fields.filter(({ name }) => listNamesInGqlObject.includes(name));
-        const nonListObjects = fields.filter(({ name }) => !listNamesInGqlObject.includes(name));
+    // Create 3 columns for each list object
+    const objectCsvData = listObjects.map(({ name }) => {
+      return [1, 2, 3].map(i => ({ [`${name}${i}`]: {} }));
+    });
 
-        // Create 3 columns for each list object
-        const objectCsvData = listObjects.map(({ name }) => {
-            return [1, 2, 3].map(i => ({ [`${name}${i}`]: {} }));
-        });
+    const nonObjectCsvData = nonListObjects.map(({ name }) => ({ [name]: {} }));
 
-        const nonObjectCsvData = nonListObjects.map(({ name }) => ({ [name]: {} }));
+    return [...nonObjectCsvData, ...objectCsvData.flat()];
+  }
 
-        return [...nonObjectCsvData, ...objectCsvData.flat()];
-    }
+  function createCsv(fileName, data) {
+    const newWorkbook = XLSX.utils.book_new();
 
-    function createCsv(fileName, data) {
-        const newWorkbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(newWorkbook, worksheet);
 
-        const worksheet = XLSX.utils.json_to_sheet(data);
-        XLSX.utils.book_append_sheet(newWorkbook, worksheet);
-        
-        XLSX.writeFile(newWorkbook, `${fileName}.csv`);
-    }
+    XLSX.writeFile(newWorkbook, `${fileName}.csv`);
+  }
 
-    function loadCsv(csv) {
-        const workbook = XLSX.read(csv, {
-            type: 'binary'
-        });
+  function downloadCsvWithData(fileName, data, typeName) {
+    const newWorkbook = XLSX.utils.book_new();
+    const entry = data[0];
+    const listNames = _.keys(entry).filter(key => Array.isArray(entry[key]));
 
-        const firstSheetName = workbook.SheetNames[0];
-        const csvRows = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName]);
+    const lists = {};
 
-        return csvRows;
-    }
+    listNames.forEach(name => {
+      lists[name] = data.flatMap(entry => entry[name]);
+    });
 
-    function getListNamesOfGqlObject(gqlObjectFields) {
-        return gqlObjectFields
-            .filter(({ type }) => type.kind === "LIST")
-            .map(({ name }) => name);
-    }
+    addSheetToWorkbook(typeName, newWorkbook, data);
 
-    return {
-        getCsvHeader,
-        downloadCsv: createCsv,
-        uploadCsv: loadCsv
-    };
+    _.keys(lists).forEach(listName => {
+      addSheetToWorkbook(listName, newWorkbook, lists[listName]);
+    });
+
+    XLSX.writeFile(newWorkbook, `${fileName}.xlsx`);
+  }
+
+  function loadCsv(csv) {
+    const workbook = XLSX.read(csv, {
+      type: "binary"
+    });
+
+    const firstSheetName = workbook.SheetNames[0];
+    const csvRows = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName]);
+
+    return csvRows;
+  }
+
+  function getListNamesOfGqlObject(gqlObjectFields) {
+    return gqlObjectFields
+      .filter(({ type }) => type.kind === "LIST")
+      .map(({ name }) => name);
+  }
+
+  function addSheetToWorkbook(sheetName, workbook, data) {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
+  }
+
+  return {
+    getCsvHeader,
+    downloadCsv: createCsv,
+    downloadCsvWithData,
+    uploadCsv: loadCsv
+  };
 }
--- src/components/Excelerator/Generator/CsvGenerator.js
+++ src/components/Excelerator/Generator/CsvGenerator.js
@@ -1,96 +1,146 @@
-import React, { useEffect, useState } from 'react';
-import { useLazyQuery, useQuery } from '@apollo/react-hooks';
-import { GET_TYPE, GET_ALL_TYPES } from '../../../graphql/SchemaGql';
-import { FormControl, InputLabel, Select, MenuItem, Button, SnackbarContent } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { useLazyQuery, useQuery } from "@apollo/react-hooks";
+import { GET_TYPE, GET_ALL_TYPES } from "../../../graphql/SchemaGql";
+import { User } from "../../../graphql/UserGql";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  SnackbarContent
+} from "@material-ui/core";
 
-import './csv-generator.scss';
-import ProgressBar from '../../Progress/ProgressBar';
-import Excelerator from '../Excelerator';
-import ErrorIcon from '@material-ui/icons/Error';
+import "./csv-generator.scss";
+import ProgressBar from "../../Progress/ProgressBar";
+import Excelerator from "../Excelerator";
+import ErrorIcon from "@material-ui/icons/Error";
+import gql from "graphql-tag";
 
 const excelerator = new Excelerator();
-const unwantedGqlTypes = ['Query', 'Mutation', "Subscription"];
+const unwantedGqlTypes = ["Query", "Mutation", "Subscription"];
+
+const GET_ALL_USERS_QUERY = gql`
+  {
+    allUsers {
+      id
+      firstName
+      lastName
+      email
+      avatar
+      children {
+        id
+        name
+        age
+      }
+    }
+  }
+`;
 
 export default function CsvGenerator() {
+  const { loading, data, error } = useQuery(GET_ALL_TYPES);
+  const [
+    getType,
+    { loading: typeLoading, data: typeToGenerate }
+  ] = useLazyQuery(GET_TYPE);
+  const { loading: userLoading, data: users } = useQuery(GET_ALL_USERS_QUERY);
+  const [inputObjects, setInputObjects] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
+  const [selectedInputObject, setSelectedInputObject] = useState("");
 
-    const { loading, data, error } = useQuery(GET_ALL_TYPES);
-    const [getType, { loading: typeLoading, data: typeToGenerate }] = useLazyQuery(GET_TYPE);
-    const [inputObjects, setInputObjects] = useState([]);
-    const [selectedInputObject, setSelectedInputObject] = useState('');
-
-    useEffect(() => {
-        if (data) {
-            const inputObjectsInSchema = getInputObjects(data.__schema);
-
-            setInputObjects(inputObjectsInSchema);
-            setSelectedInputObject(inputObjectsInSchema[0]);
-        }
-    }, [data])
-
-    useEffect(() => {
-        if (selectedInputObject) {
-            getType({ variables: { typeName: selectedInputObject.name } });
-        }
-    }, [getType, selectedInputObject])
-
-    const generateExcelFile = () => {
-        if (typeToGenerate && typeToGenerate.__type) {
-            const { name, inputFields } = typeToGenerate.__type;
-
-            const header = excelerator.getCsvHeader(inputFields);
-
-            excelerator.downloadCsv(name, header);
-        }
-    };
-
-    if (error) {
-        return <div className="snackbar-container">
-            <SnackbarContent
-                className="error-snackbar"
-                message={
-                    <span id="client-snackbar">
-                        <ErrorIcon /> Unable to generate excel file, error loading GQL types
-                    </span>
-                }
-            />
-        </div>
+  useEffect(() => {
+    if (users) {
+      setAllUsers(users);
     }
+  }, [users]);
 
-    if (loading || typeLoading) {
-        return <ProgressBar />
+  useEffect(() => {
+    if (data) {
+      const inputObjectsInSchema = getInputObjects(data.__schema);
+
+      setInputObjects(inputObjectsInSchema);
+      setSelectedInputObject(inputObjectsInSchema[0]);
     }
+  }, [data]);
 
+  useEffect(() => {
+    if (selectedInputObject) {
+      getType({ variables: { typeName: selectedInputObject.name } });
+    }
+  }, [getType, selectedInputObject]);
+
+  const generateExcelFile = () => {
+    if (typeToGenerate && typeToGenerate.__type) {
+      const users = allUsers.allUsers.map(user => new User(user));
+      const { name, inputFields } = typeToGenerate.__type;
+      const header = excelerator.getCsvHeader(inputFields);
+
+      //   excelerator.downloadCsv(name, header);
+      excelerator.downloadCsvWithData(name, users, "Users");
+    }
+  };
+
+  if (error) {
     return (
-        <div className="csv-generator">
-            <div className="csv-generator-container">
-                <div className="types-selector">
-                    <FormControl style={{ minWidth: '180px' }}>
-                        <InputLabel id="gql-type">Type</InputLabel>
-                        <Select
-                            id="gql-type-select"
-                            value={selectedInputObject}
-                            onChange={({ target }) => setSelectedInputObject(target.value)}
-                        >
-                            {inputObjects.map(type => <MenuItem key={type.name} value={type}>{type.name}</MenuItem>)}
-                        </Select>
-                    </FormControl>
-                </div>
-                
-                <div id="choose-type-btn">
-                    <Button onClick={generateExcelFile} variant="outlined" color="primary">
-                        Generate excel file
-                    </Button>
-                </div>
-            </div>
-        </div>
+      <div className="snackbar-container">
+        <SnackbarContent
+          className="error-snackbar"
+          message={
+            <span id="client-snackbar">
+              <ErrorIcon /> Unable to generate excel file, error loading GQL
+              types
+            </span>
+          }
+        />
+      </div>
     );
+  }
+
+  if (loading || typeLoading) {
+    return <ProgressBar />;
+  }
+
+  return (
+    <div className="csv-generator">
+      <div className="csv-generator-container">
+        <div className="types-selector">
+          <FormControl style={{ minWidth: "180px" }}>
+            <InputLabel id="gql-type">Type</InputLabel>
+            <Select
+              id="gql-type-select"
+              value={selectedInputObject}
+              onChange={({ target }) => setSelectedInputObject(target.value)}
+            >
+              {inputObjects.map(type => (
+                <MenuItem key={type.name} value={type}>
+                  {type.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div id="choose-type-btn">
+          <Button
+            onClick={generateExcelFile}
+            variant="outlined"
+            color="primary"
+          >
+            Generate excel file
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function getInputObjects(gqlSchema) {
-    const { types } = gqlSchema;
+  const { types } = gqlSchema;
 
-    return types.filter(type =>
-        !type.name.startsWith('__') &&
-        !unwantedGqlTypes.includes(type.name) &&
-        type.kind === 'INPUT_OBJECT');
+  return types.filter(
+    type =>
+      !type.name.startsWith("__") &&
+      !unwantedGqlTypes.includes(type.name) &&
+      type.kind === "INPUT_OBJECT"
+  );
 }
